b18a6f638484102727c6799d7806a04df6791b2a
Andy M <admazzola@gmail.com>
2017-11-06 16:11:52 -0500

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+
+
+var phaser = require('phaser')
+
+requirejs(["internal/game/game"], function(game) {
+
+});
+
+
+  requirejs(["internal/client-render/client-scene"], function(scene) {
+
+    });
